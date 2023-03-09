3ab9914859be564b5c6b496234fc4f4880a7571e
你想要的是咸菜 <63651801+decike@users.noreply.github.com>
2023-03-09 22:25:53 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/ysc3839/openwrt-minieap.git -b gzhu package/minieap
+git clone https://github.com/ysc3839/luci-proto-minieap.git package/luci-proto-minieap
